c5814f02933c5a89b37b55f9e5adc5f031323357
Adrian Jaraine <adrianjaraine12@gmail.com>
2021-11-18 04:30:27 +0530

    v1.2. Added get_close_matches

--- dictionary.py
+++ dictionary.py
@@ -1,4 +1,5 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("dictionary.json"))
 
@@ -10,6 +11,15 @@ def translate(word):
         return data[word.title()]
     elif word.upper() in data: #checks if word exists in Upper case
         return data[word.upper()]
+    elif len(get_close_matches(word, data.keys())) > 0:
+        print("did you mean %s instead" %get_close_matches(word, data.keys())[0])
+        decide = input("Press y for yes and n for no")
+        if decide == "y":
+            return data[get_close_matches(word, data.keys())[0]]
+        elif decide == "n":
+            return("oops")
+        else:
+            return("You have entered wrong input, please enter y or n")
     else:
         print('You have entered the wrong word')
 
